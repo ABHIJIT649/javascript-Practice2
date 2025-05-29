a0c48d9cf96993c797ddcadaf04c3d59b37c77b8
ABHIJIT649 <abhijitmohanty440@gmail.com>
2025-05-30 01:01:51 +0530

    practice dom

--- daytwo/chpr6 DOM/dom.js
+++ daytwo/chpr6 DOM/dom.js
@@ -1,1 +1,18 @@
-alert("error")
+
+// document.body.style.background="red";
+// document.body.childNodes[1].innerText = "abcd";
+let heading= document.getElementById("heading") //value return
+console.log(heading);
+
+
+let headings = document.getElementsByClassName("heading2");
+console.dir(headings)
+console.log(headings);//return html collection
+
+
+let paras = document.getElementsByTagName("p") // first element
+console.log(paras);//return paraghraph element 
+console.dir(paras)
+
+ let element = document.querySelector("#myId");
+ console.dir(element)
